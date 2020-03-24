4b7d2fa1a6a298b0e8e3a1f2d04a6b466622a454
Jose Miguel <jmzegers@miuandes.cl>
2020-03-24 02:58:07 -0300

    Intentando

---
+++ lab01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Laboratorio_1_POO_jmzegers
+{
+    public class Jugador
+    {
+        public int resultado;
+
+        static int Lanzar(int a, int b)
+        {
+            Random rnd = new Random();
+            return rnd.Next(a, b);
+
+        }
+
+        public string nombre;
+        public string apellido;
+        private static int cachipun;
+        private static int res;
+
+        public Jugador(string nJugador, string aJugador)
+        {
+            nJugador = nombre;
+            aJugador = apellido;
+        }
+
+        static void Main(string[] args)
+        {
+            Jugador jugador1 = new Jugador("Bob", "Kunga");
+            res = Jugador.Lanzar(0, 3);
+            Console.WriteLine(res);
+            Console.WriteLine(jugador1);
+        }
+    }
+
+}
